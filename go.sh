d9ffe245f148c9d26dabf777fb5a714dcebf7149
Omer Tuchfeld <omer@tuchfeld.dev>
2023-01-04 02:31:29 +0100

    Better

--- go.sh
+++ go.sh
@@ -12,90 +12,24 @@ echo 'function data() { return (' > $SCRIPT_DIR/data.js
 now=$(jq -n 'now | todateiso8601' -r)
 
 cat $SCRIPT_DIR/audit/*audit* | jq --arg now "$now" -r --slurp '
+. as $root |
 [
-    {
-        "group": "pods",
+    ["pods", "deployments", "secrets", "configmaps"][]
+    | . as $type
+    | {
+        "group": $type,
         "data": [
-            .[] 
+            $root[] 
             | select(
                 .verb == "create"
-                and .objectRef.resource == "pods"
+                and .objectRef.resource == $type
                 and .responseStatus.code == 201
                 and .objectRef.resource != "subjectaccessreviews"
                 and .objectRef.resource != "tokenreviews"
                 and .objectRef.name != null
             ) 
             | {
-                "label": .objectRef.name,
-                "data": [
-                    {
-                        "timeRange": [ .requestReceivedTimestamp, $now ],
-                        "val": .objectRef.namespace
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        "group": "deployments",
-        "data": [
-            .[] 
-            | select(
-                .verb == "create"
-                and .objectRef.resource == "deployments"
-                and .responseStatus.code == 201
-                and .objectRef.resource != "subjectaccessreviews"
-                and .objectRef.resource != "tokenreviews"
-                and .objectRef.name != null
-            ) 
-            | {
-                "label": .objectRef.name,
-                "data": [
-                    {
-                        "timeRange": [ .requestReceivedTimestamp, $now ],
-                        "val": .objectRef.namespace
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        "group": "secrets",
-        "data": [
-            .[] 
-            | select(
-                .verb == "create"
-                and .objectRef.resource == "secrets"
-                and .responseStatus.code == 201
-                and .objectRef.resource != "subjectaccessreviews"
-                and .objectRef.resource != "tokenreviews"
-                and .objectRef.name != null
-            ) 
-            | {
-                "label": .objectRef.name,
-                "data": [
-                    {
-                        "timeRange": [ .requestReceivedTimestamp, $now ],
-                        "val": .objectRef.namespace
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        "group": "configmaps",
-        "data": [
-            .[] 
-            | select(
-                .verb == "create"
-                and .objectRef.resource == "configmaps"
-                and .responseStatus.code == 201
-                and .objectRef.resource != "subjectaccessreviews"
-                and .objectRef.resource != "tokenreviews"
-                and .objectRef.name != null
-            ) 
-            | {
-                "label": .objectRef.name,
+                "label": "\(.objectRef.namespace)/\(.objectRef.name)",
                 "data": [
                     {
                         "timeRange": [ .requestReceivedTimestamp, $now ],
