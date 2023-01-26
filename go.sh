33dfd96792eb8f0c6137e6ee4cc1a5c17a04ff72
Omer Tuchfeld <omer@tuchfeld.dev>
2023-01-26 10:52:48 +0100

    Merge with API availability

--- go.sh
+++ go.sh
@@ -4,34 +4,48 @@ set -euxo pipefail
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
+mv audit audit_old_$(date -u +%Y-%m-%dT%H:%M:%S%Z) || true
 mkdir -p $SCRIPT_DIR/audit
-oc adm node-logs $(oc get nodes -oname) --path=kube-apiserver/ | grep audit | while read log; do oc adm node-logs master1 --path=kube-apiserver/$log > audit/$log; done
-
-echo 'function data() { return (' > $SCRIPT_DIR/data.js
+oc adm node-logs $(oc get nodes -oname) --path=kube-apiserver/ | grep audit | while read log; do oc adm node-logs master1 --path=kube-apiserver/$log >audit/$log; done
 
 now=$(jq -n 'now | todateiso8601' -r)
 
 cat $SCRIPT_DIR/audit/*audit* | jq --arg now "$now" -r --slurp '
 . as $root 
-| ([.[].objectRef.namespace] | unique) as $namespaces
 | [
-    $namespaces[]
-    | . as $namespace
-    | {
-        "group": $namespace,
+    {
+        "group": "audit",
         "data": [
             $root[]
             | select(
                 .verb == "create"
-                and (.objectRef.resource | test("(pods|deployments|secrets|configmaps)"))
-                and (.objectRef.namespace == $namespace)
+                and (.objectRef.resource | test("(pods|deployments|secrets|configmaps|namespaces)"))
                 and .responseStatus.code == 201
-                and .objectRef.resource != "subjectaccessreviews"
-                and .objectRef.resource != "tokenreviews"
                 and .objectRef.name != null
             ) 
             | {
-                "label": "\(.objectRef.name) - \(.objectRef.resource)",
+                "label": "\(.objectRef.namespace) - \(.objectRef.name) - \(.objectRef.resource)",
+                "data": [
+                    {
+                        "timeRange": [ .requestReceivedTimestamp, $now ],
+                        "val": .objectRef.resource
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "group": "events",
+        "data": [
+            $root[]
+            | select(
+                .verb == "create"
+                and (.objectRef.resource | test("(events)"))
+                and .responseStatus.code == 201
+                and .objectRef.name != null
+            ) 
+            | {
+                "label": "\(.objectRef.namespace) - \(.objectRef.name) - \(.objectRef.resource)",
                 "data": [
                     {
                         "timeRange": [ .requestReceivedTimestamp, $now ],
@@ -42,5 +56,10 @@ cat $SCRIPT_DIR/audit/*audit* | jq --arg now "$now" -r --slurp '
         ]
     }
 ]
-' >> data.js
-echo ')}' >> data.js
+' >data.json
+
+AVAILABILITY_DATA="$SCRIPT_DIR/../kube-api-availability/static/data.json"
+if [[ -f $AVAILABILITY_DATA ]]; then
+	# Merge the AVAILABILITY_DATA JSON array with our data.js JSON array
+	jq -s '.[0] + .[1]' "$AVAILABILITY_DATA" data.json | sponge data.json
+fi
---
+++ show.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+google-chrome --allow-file-access-from-files --user-data-dir="$SCRIPT_DIR"/chrome "$SCRIPT_DIR"/index.html
